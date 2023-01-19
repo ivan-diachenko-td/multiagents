acecaad8b53ab9ce02b9fba0c5bf40776450c9ea
Ivan Diachenko <ivan.diachenko@teamdev.com>
2023-01-19 19:39:01 +0200

    Add lab3-4

---
+++ src/nure/diachenko/Room.java
@@ -0,0 +1,40 @@
+package nure.diachenko;
+
+public class Room {
+    private int x = 1;
+    private int y = 1;
+
+    public Room(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o != null && o instanceof Room r) {
+            return x == r.x && y == r.y;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + getX();
+        result = 43 * result + getY();
+        return result;
+    }
+}
